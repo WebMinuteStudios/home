144971c37654ac2af95934ce5129d3b97fd4691e
sfoux <saahirhfoux@gmail.com>
2015-12-19 16:31:30 -0500

    Wrapping markup within fullPage.js section & slide elements

---
+++ js/webminute.js
@@ -0,0 +1,9 @@
+jQuery(function ($) {
+    "use strict";
+    $('#fullpage').fullpage({
+        css3: true,
+        loopHorizontal: false,
+        controlArrows: false,
+        scrollingSpeed: 800
+    });
+});
